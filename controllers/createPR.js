1616cd21e40e3f66f9d889f72d3755ca5dc378a4
SUBID DAS <subid.das@awesomesuite.com>
2024-03-03 04:20:37 +0530

    title, body are optional

--- controllers/createPR.js
+++ controllers/createPR.js
@@ -4,7 +4,12 @@ require("dotenv").config();
 const token = process.env.GITHUB_TOKEN;
 
 const createPR = async (req, res, sourceBranch, destinationBranch) => {
-  const { owner, repo, title, body } = req.body;
+  const {
+    owner,
+    repo,
+    title = `Updated ${req.body.filePath}`,
+    body = `Modified element(s) with ${req.body.id} in ${req.body.filePath}`,
+  } = req.body;
   const data = {
     title,
     base: destinationBranch,
