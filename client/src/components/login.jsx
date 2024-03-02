5074c78967ef9521e40649a6572f5056ddd989e2
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-03-03 00:52:54 +0530

    Update login and signup components and add background images

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -12,6 +12,11 @@ const Login = () => {
 				</div>
 				<div className='flex justify-center items-center'>
 					<Link
+						to='/'
+						className='bg-slate-950 px-5 py-2 rounded-full font-semibold hover:border-2 hover:border-white'>
+						Home
+					</Link>
+					<Link
 						to='/docs'
 						className='bg-slate-950 px-5 py-2 rounded-full font-semibold hover:border-2 hover:border-white'>
 						Docs
@@ -30,18 +35,21 @@ const Login = () => {
 			</nav>
 			<section className='flex justify-center items-center min-h-screen my-10'>
 				<div className='w-3/4 h-screen bg-gray-200/50 shadow-white rounded-3xl flex '>
-					<div className='p-10 w-1/2 h-full bg-white rounded-l-3xl text-black flex flex-col items-center justify-center'>
-						<div className='w-full'>
+					<div className='w-1/2 h-full bg-black rounded-l-3xl flex justify-center items-center'>
+						<div className='w-3/4 text-white'>
 							<img
 								src={Logo}
 								alt=''
 								className='w-20 mb-4 flex justify-center items-center m-auto'
 							/>
-							<h1 className='text-3xl font-semibold mb-4 italic'>
-								Create Account
+							<h1 className='text-4xl font-semibold mb-4 italic'>
+								Welcome Back
 							</h1>
+							<p className='text-lg'>
+								To keep connected with us please login with your credentials
+							</p>
 							<label
-								className='block text-gray-700 text-sm font-bold mb-2 text-left'
+								className='block text-white text-sm font-bold mb-2 text-left mt-5'
 								htmlFor='email'>
 								Email
 							</label>
@@ -49,35 +57,27 @@ const Login = () => {
 								type='email'
 								id='email'
 								placeholder='Email'
-								className='mb-4 p-2 border rounded w-full'
+								className='mb-4 p-2 border rounded w-full outline-none'
 							/>
 							<label
-								className='block text-gray-700 text-sm font-bold mb-2 text-left'
+								className='block text-white text-sm font-bold mb-2 text-left'
 								htmlFor='password'>
-								Enter your Password
+								Password
 							</label>
 							<input
 								type='password'
 								id='password'
 								placeholder='Password'
-								className='mb-4 p-2 border rounded w-full'
+								className='mb-4 p-2 border rounded w-full outline-none'
 							/>
-							<label
-								className='block text-gray-700 text-sm font-bold mb-2 text-left'
-								htmlFor='password'>
-								Re-enter your Password to verify
-							</label>
-							<input
-								type='password'
-								id='password'
-								placeholder='Password'
-								className='mb-4 p-2 border rounded w-full'
-							/>
-							<button className='bg-black text-white p-2 rounded-3xl w-3/4 font-semibold flex justify-center items-center m-auto'>
-								Sign Up
-							</button>
+							<Link to='/login'>
+								<button className='bg-white text-black p-2 rounded-3xl w-1/2 mt-4 font-semibold flex justify-center items-center m-auto'>
+									Log In
+								</button>
+							</Link>
 						</div>
 					</div>
+					<div className='w-1/2 min-h-screen'></div>
 				</div>
 			</section>
 		</>
--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -12,6 +12,11 @@ const Signup = () => {
 				</div>
 				<div className='flex justify-center items-center'>
 					<Link
+						to='/'
+						className='bg-slate-950 px-5 py-2 rounded-full font-semibold hover:border-2 hover:border-white'>
+						Home
+					</Link>
+					<Link
 						to='/docs'
 						className='bg-slate-950 px-5 py-2 rounded-full font-semibold hover:border-2 hover:border-white'>
 						Docs
@@ -30,21 +35,13 @@ const Signup = () => {
 			</nav>
 			<section className='flex justify-center items-center min-h-screen my-10'>
 				<div className='w-3/4 h-screen bg-gray-200/50 shadow-white rounded-3xl flex '>
-					<div className='w-1/2 h-full bg-black rounded-l-3xl flex justify-center items-center'>
-						<div className='w-3/4 text-white'>
-							<img
-								src={Logo}
-								alt=''
-								className='w-20 mb-4 flex justify-center items-center m-auto'
-							/>
-							<h1 className='text-4xl font-semibold mb-4 italic'>
-								Welcome Back
-							</h1>
-							<p className='text-lg'>
-								To keep connected with us please login with your credentials
-							</p>
+					<div className='p-10 w-1/2 h-full bg-white rounded-l-3xl text-black flex flex-col items-center justify-center'>
+						<img src={Logo} alt='' className='w-20 mb-4 m-auto' />
+						<h1 className='text-4xl font-semibold mb-1'>Create Account</h1>
+						<h1>And gain access to our service</h1>
+						<div className='w-full'>
 							<label
-								className='block text-white text-sm font-bold mb-2 text-left mt-5'
+								className='block text-gray-700 text-sm font-bold mb-2 text-left'
 								htmlFor='email'>
 								Email
 							</label>
@@ -52,26 +49,47 @@ const Signup = () => {
 								type='email'
 								id='email'
 								placeholder='Email'
-								className='mb-4 p-2 border rounded w-full outline-none'
+								className='mb-4 p-2 border rounded w-full'
 							/>
 							<label
-								className='block text-white text-sm font-bold mb-2 text-left'
+								className='block text-gray-700 text-sm font-bold mb-2 text-left'
+								htmlFor='github'>
+								Github
+							</label>
+							<input
+								type='email'
+								id='email'
+								placeholder='Github Username'
+								className='mb-4 p-2 border rounded w-full'
+							/>
+							<label
+								className='block text-gray-700 text-sm font-bold mb-2 text-left'
 								htmlFor='password'>
-								Password
+								Enter your Password
 							</label>
 							<input
 								type='password'
 								id='password'
 								placeholder='Password'
-								className='mb-4 p-2 border rounded w-full outline-none'
+								className='mb-4 p-2 border rounded w-full'
 							/>
-							<Link to='/login'>
-								<button className='bg-white text-black p-2 rounded-3xl w-1/2 mt-4 font-semibold flex justify-center items-center m-auto'>
-									Log In
-								</button>
-							</Link>
+							<label
+								className='block text-gray-700 text-sm font-bold mb-2 text-left'
+								htmlFor='password'>
+								Re-enter your Password to verify
+							</label>
+							<input
+								type='password'
+								id='password'
+								placeholder='Password'
+								className='mb-4 p-2 border rounded w-full'
+							/>
+							<button className='bg-black text-white p-2 rounded-3xl w-3/4 font-semibold flex justify-center items-center m-auto'>
+								Sign Up
+							</button>
 						</div>
 					</div>
+					<div className='bg-black w-1/2 min-h-screen flex flex-col justify-center items-center m-auto relative rounded-r-3xl bg-signup-image bg-cover bg-center'></div>
 				</div>
 			</section>
 		</>
