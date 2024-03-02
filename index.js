f89ce0950a85cd17152dd8b8e1b92ba6a622588c
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2024-03-02 16:05:07 +0530

    server modificaitons

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const express = require('express');
+const express = require("express");
+const cors = require('cors');
+
+// Create an Express application
 const app = express();
+
+// Use middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Default route
+app.get("/", (req, res) => {
+    res.send("Welcome to the server!");
+});
+
+
+// Start the Express server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+});
