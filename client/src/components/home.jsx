d8e77d8deabee2dc5e4b0edfd07012700602e58e
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-03-02 19:21:14 +0530

    home.jsx done

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,9 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import Logo from "../assets/logo.png";
+import Typed from "typed.js";
 const Home = () => {
+	const el = React.useRef(null);
+	React.useEffect(() => {
+		const typed = new Typed(el.current, {
+			strings: ["$ npm install lorem"],
+			typeSpeed: 70,
+			loop: true,
+		});
+		return () => {
+			// Destroy Typed instance during cleanup to stop animation
+			typed.destroy();
+		};
+	}, []);
 	return (
 		<>
-			<nav className='flex justify-between items-center max-w-full h-16 bg-slate-950 text-white px-5'>
+			<nav className='flex justify-between items-center max-w-full h-16 bg-slate-950 text-white px-5 '>
 				<div className='flex items-center'>
 					<img src={Logo} alt='' className='w-10 mx-3' />
 					<h1 className='font-semibold'>HackNITR</h1>
@@ -20,21 +33,27 @@ const Home = () => {
 					</button>
 				</div>
 			</nav>
-			<div className='flex flex-col justify-center items-center  lg:px-56 md:px-48 sm:px-36 px-24 mt-5 flex-wrap'>
-				<h1 className='md:text-7xl sm:text-5xl text-4xl font-semibold'>
-					Lorem ipsum dolor sit amet.
-				</h1>
-				<p className='text-lg mt-5 text-gray-500'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
-					molestias maxime minus obcaecati similique quam.
-				</p>
-				<button className='bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded mt-10 duration-300 w-1/4'>
-					Get Started
-				</button>
-				<h1 className='text-gray-400 text-xl'>
-					View installation instructions &rarr;
-				</h1>
-			</div>
+			<section className='flex justify-center items-center min-h-screen'>
+				<div className='flex flex-col justify-center items-center lg:px-52 md:px-48 sm:px-36 px-24 flex-wrap'>
+					<h1 className='md:text-7xl sm:text-5xl text-4xl font-semibold'>
+						Lorem ipsum dolor sit amet.
+					</h1>
+					<p className='text-lg mt-5 text-gray-500'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+						molestias maxime minus obcaecati similique quam.
+					</p>
+					<button className='bg-blue-500 hover:bg-blue-700 font-bold py-2 md:px-4 rounded mt-10 duration-300 w-1/2 sm:w-1/3 md:1/4'>
+						Get Started
+					</button>
+					<h1 className='text-gray-400 text-xl'>
+						View installation instructions &rarr;
+					</h1>
+					<div className='my-10 text-2xl'>
+						<span ref={el} />
+					</div>
+					<h1 className='font-semibold text-4xl'>View all our commands</h1>
+				</div>
+			</section>
 		</>
 	);
 };
