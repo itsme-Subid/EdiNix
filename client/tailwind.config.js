cd8d1412bc6e24548d0b3bf03af12e8049fcbd16
SUBID DAS <subid.das@awesomesuite.com>
2024-03-03 06:27:19 +0530

    Update

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {
-			backgroundImage: (theme) => ({
-				"login-image":
-					"url(https://aliabdaal.com/wp-content/uploads/2023/01/DATABASE-01.png)",
-				"signup-image":
-					"url(https://cdnp3.stackassets.com/c88d10bd2ea99376acfdd1d9212fb5b91882a59f/store/opt/720/541/8ac7173b8dbacac921d1bf3c9e999e7f8558ee2b80968ac525502533238a/product_332340_product_shots1.jpg)",
-			}),
-		},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: () => ({
+        "login-image":
+          "url(https://aliabdaal.com/wp-content/uploads/2023/01/DATABASE-01.png)",
+        "signup-image":
+          "url(https://cdnp3.stackassets.com/c88d10bd2ea99376acfdd1d9212fb5b91882a59f/store/opt/720/541/8ac7173b8dbacac921d1bf3c9e999e7f8558ee2b80968ac525502533238a/product_332340_product_shots1.jpg)",
+      }),
+    },
+  },
+  plugins: [],
 };
